e35f4e3f27034c51d632bc910f86154ca48b860b
FilippoFoti <filippofoti7@gmail.com>
2023-06-14 18:06:36 +0200

    Add card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    },
+    data() {
+        return {
+            baseUrl: 'http://localhost:8000',
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <img v-if="project.image" :src="`${baseUrl}/storage/${project.image}`" class="card-img-top" alt="project.title">
+        <div v-else>Nessuna immagine</div>
+        <div class="card-body">
+            <h5>{{ project.title }}</h5>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
