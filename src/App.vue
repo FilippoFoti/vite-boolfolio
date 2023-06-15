52ffb60dea46e360915586fdd8a116b7c638f033
FilippoFoti <filippofoti7@gmail.com>
2023-06-15 15:30:22 +0200

    Add Pagination, type and technology in card

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,36 @@ export default {
   data() {
     return {
       baseUrl: "http://localhost:8000",
-      projects: []
+      projects: [],
+      currentPage: 1,
+      lastPage: null,
+      totalProjects: 0,
+      loading: false,
     };
   },
   mounted() {
     this.getProjects();
   },
   methods: {
-    getProjects() {
-      axios.get(`${this.baseUrl}/api/projects`).then(resp => {
-        this.projects = resp.data.results;
-      });
+    // getProjects() {
+    //   axios.get(`${this.baseUrl}/api/projects`).then(resp => {
+    //     this.projects = resp.data.results.data;
+    //   });
+    // }
+    getProjects(pageNumber = 1) {
+      this.loading = true;
+      axios.get(`${this.baseUrl}/api/projects`, {
+        params: {
+          page: pageNumber
+        }
+      }).then(resp => {
+        this.projects = resp.data.results.data;
+        this.currentPage = resp.data.results.current_page;
+        this.lastPage = resp.data.results.last_page;
+        this.totalProjects = resp.data.results.total;
+      }).finally(() => {
+        this.loading = false;
+      })
     }
   },
   components: { ProjectCard }
@@ -25,11 +44,30 @@ export default {
 
 <template>
   <div class="container">
-    <h2>Lista dei Progetti</h2>
-    <div class="row row-cols-3 g-3">
-      <div class="col" v-for="project in projects" :key="project.id">
-        <ProjectCard :project="project" />
+    <div v-if="!loading">
+      <h2 class="m-0 py-3">Lista dei Progetti</h2>
+      <div class="text-start">
+        <p>Trovati {{ totalProjects }} progetti</p>
       </div>
+      <div class="row row-cols-3 g-3">
+        <div class="col" v-for="project in projects" :key="project.id">
+          <ProjectCard :project="project" />
+        </div>
+      </div>
+
+      <nav v-if="lastPage" class="m-4 d-flex justify-content-center" aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item" :class="{ 'disabled': currentPage === 1 }"><a
+              @click.prevent="getProjects(currentPage - 1)" class="page-link" href="#">Previous</a></li>
+          <li class="page-item" :class="{ 'active': pageNumber === currentPage }" v-for="pageNumber in lastPage"><a @click.prevent="getProjects(pageNumber)" class="page-link" href="#">{{ pageNumber }}</a></li>
+          <li class="page-item" :class="{ 'disabled': currentPage === lastPage }"><a
+              @click.prevent="getProjects(currentPage + 1)" class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
+
+    </div>
+    <div v-else>
+      <p>Loading...</p>
     </div>
   </div>
 </template>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -8,6 +8,15 @@ export default {
         return {
             baseUrl: 'http://localhost:8000',
         }
+    },
+    computed: {
+        contentPreview() {
+            if (this.project.content.length > 50) {
+                return this.project.content.substring(0, 50) + "...";
+            } else {
+                return this.project.content
+            }
+        }
     }
 }
 </script>
@@ -15,12 +24,15 @@ export default {
 <template>
     <div class="card h-100">
         <img v-if="project.image" :src="`${baseUrl}/storage/${project.image}`" class="card-img-top" alt="project.title">
-        <div v-else>Nessuna immagine</div>
+        <div v-else class="p-3">Nessuna immagine</div>
         <div class="card-body">
             <h5>{{ project.title }}</h5>
+            <p>{{ contentPreview }}</p>
+            <p v-if="project.type">{{ project.type.name }}</p>
+            <p v-else>Nessuna tipologia</p>
+            <p v-for="technology in project.technologies">{{ technology.name }}</p>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
